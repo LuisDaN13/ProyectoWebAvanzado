475d6bbf1b9a3697152f68822c42170778fcafc5
lnune80132 <nunez9076@gmail.com>
2024-07-18 19:51:10 -0600

    PrimerPush

---
+++ pro_GestorTareasProyectos/pro_GestorTareasProyectos/Controllers/ProyectoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pro_GestorTareasProyectos.Controllers
+{
+    public class ProyectoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CrearProyecto()
+        {
+            return View();
+        }
+        public IActionResult VerProyectos()
+        {
+            return View();
+        }
+
+        public IActionResult VerTareas()
+        {
+            return View();
+        }
+    }
+}
---
+++ pro_GestorTareasProyectos/pro_GestorTareasProyectos/Controllers/UsuarioController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pro_GestorTareasProyectos.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
